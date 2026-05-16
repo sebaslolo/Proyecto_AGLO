e68999102ac1fbe18a2bdba7783fda10aa22865e
sebaslolo <sebaslopez2009@gmail.com>
2026-05-16 15:11:52 -0600

    Commit 0: inicializacion

---
+++ src/main/java/com/Proyecto_Grupo_1/ProyectoGrupo1Application.java
@@ -0,0 +1,13 @@
+package com.Proyecto_Grupo_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGrupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGrupo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyecto_Grupo_1/ProyectoGrupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto_Grupo_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGrupo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
